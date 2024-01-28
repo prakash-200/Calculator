f4d3b98050a45a2d5bc8faba0e6838eb8b7a5705
prakash-200 <prakashrajrko1@gmail.com>
2024-01-28 18:56:44 +0530

    finished

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
 
 function show(value){
-    document.getElementById('display').value += value
+    display.value += value
     console.log(display.value)
 }
 
 function calculate(){
-    document.getElementById('display').value = eval(document.getElementById('display').value)
+    display.value = eval(document.getElementById('display').value)
 
 }
 
 function clearValue(){
-    document.getElementById('display').value = ''
+    display.value = ''
     console.log("hello")
     
+}
+
+function change(){
+    display.value = display.value.slice(0,-1);
 }
